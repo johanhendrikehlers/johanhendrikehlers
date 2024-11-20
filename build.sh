00b68c3dd022690eb33a15615f295eb98ceb99c2
Johan Hendrik Ehlers <johan@olympiahld.com>
2024-11-20 15:21:20 +0200

    trying to get hugo version jumped up

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 npm install
+# if hugo version less than v0.139 use snap
+if [ "$(hugo version | awk '{print $5}')" \< "v0.139" ]; then
+    sudo snap install hugo --channel=extended
+fi 
 hugo version
